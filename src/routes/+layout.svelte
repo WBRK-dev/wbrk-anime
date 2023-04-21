cb97e8e78d7cbf5334280bfe96c4e0ab640052e3
WBRK-dev <koertjrweberp@gmail.com>
2023-04-21 16:00:44 +0200

    Added Custom Favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 
 <svelte:head>
     <link rel="stylesheet" href="/wbrk.css/wbrk.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <title>WBR_K Anime</title>
 </svelte:head>
 
