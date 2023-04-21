0a67f3f8bb070de61cb77083f2f16928a83be2be
WBRK-dev <koertjrweberp@gmail.com>
2023-04-21 08:33:34 +0200

    Changed Adapter to Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,12 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: null,
-            precompress: false,
-            strict: true
+		adapter: adapter({
+            // See below for an explanation of these options
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
         })
     }
 };
