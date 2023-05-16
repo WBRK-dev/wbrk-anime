8dcf067a835d449441f2417287f4f2d9bbd8ef99
WBRK-dev <koertjrweberp@gmail.com>
2023-05-16 14:31:28 +0200

    Oauth test 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,19 @@
     import { onMount } from "svelte";
     import Searchbar from "../components/searchbar.svelte";
 
-    let accesstoken = "";
+    let accesstoken = ""; let code = ""
     if (browser) {
         accesstoken = localStorage.getItem("accesstoken") || "";
+        if (localStorage.getItem("code_verifier") === null) {
+            let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H",
+            "I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+            while (code.length < 50) {
+                code = code + letters[Math.floor(letters.length * Math.random())];
+            }
+            localStorage.setItem("code_verifier", code);
+        } else {
+            code = localStorage.getItem("code_verifier");
+        }
     }
 </script>
 
@@ -25,7 +35,7 @@
             <a href="/watch">Featured</a>
         </div>
         {#if accesstoken === ""}
-            <a href='./'>Login</a>
+            <a href='https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=fb5613df0f39524a295b0d1d7a6213ca&code_challenge={code}'>Login</a>
         {:else}
             <a href='account/'>Account</a>
         {/if}
