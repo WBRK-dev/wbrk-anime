579cb37a671bffa8d2178c99cdb0a9598a1f3b63
WBRK-dev <koertjrweberp@gmail.com>
2023-05-22 19:47:03 +0200

    prod

--- src/routes/authorizetoken/+page.svelte
+++ src/routes/authorizetoken/+page.svelte
@@ -5,7 +5,7 @@
     import { onMount } from 'svelte';
     
     let authorizationtoken = $page.url.searchParams.get('code');
-    let accesstoken = "Logged In"; let userinfo = {"name": "Username", "watching": 0, "completed": 0, "num_days_watched": 0}
+    let authorized = 0; let userinfo = {"name": "Username", "watching": 0, "completed": 0, "num_days_watched": 0}
 
     onMount(async () => {
         const response = await fetch("https://wbrk-anime-api.vercel.app/api/authorize?code="+authorizationtoken, {credentials: "include"});
@@ -13,16 +13,15 @@
             console.log(response);
             let json = await response.json();
             if (json.succesfull) {
-                accesstoken = "Logged In"
                 let userinfoR = await fetch("https://wbrk-anime-api.vercel.app/api/user/info", {credentials: "include"});
                 let userinfoJ = await userinfoR.json();
-                console.log(userinfoJ);
                 userinfo.name = userinfoJ.name;
-                userinfo.watching = userinfoJ.anime_statistics.num_items_watched;
+                userinfo.watching = userinfoJ.anime_statistics.num_items_watching;
                 userinfo.completed = userinfoJ.anime_statistics.num_items_completed;
                 userinfo.num_days_watched = userinfoJ.anime_statistics.num_days_watched;
+                authorized = 1;
             } else {
-                accesstoken = "Failed to login"
+                authorized = 2;
             }
         }
     })
@@ -30,9 +29,9 @@
 
 <main>
     <div class="dialog">
-        {#if accesstoken === ""}
+        {#if authorized === 0}
             <div class="spinner"><Spinner/></div>
-        {:else if accesstoken === "Logged In"}
+        {:else if authorized === 1}
             <div class="userinfo">
                 <p class="title">{userinfo.name}</p>
                 <p class="data">Completed Anime: <span>{userinfo.completed}</span></p>
@@ -41,7 +40,7 @@
                 <a href="/">Home</a>
             </div>
         {:else}
-            <div class="spinner">{accesstoken}</div>
+            <div class="spinner">Failed to login</div>
         {/if}
     </div>
 </main>
